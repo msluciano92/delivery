97c3909ab32cf1b6b433c91026e10d299998a7e3
Francisco Lopez Aranguren <francisco.lopez@arzion.com.ar>
2019-01-18 16:54:19 -0300

    Implements package endpoint

--- tests/controllers/PackageController.test.js
+++ tests/controllers/PackageController.test.js
@@ -1,14 +1,12 @@
 describe('POST /package/create-package', () => {
-
-    it('Return package received and tranferred. ', async(done)  => {
-      app
-          .post('/package/create-package')
-          .send({ date_send: '2019-01-10', gps: 'La Plata, Buenos Aires, Argentina' })
-          .except(201)
-          .end(async (err) => {
-              if (err) throw err;
-              except(201).toBe();
-          });
+    it('Return package received and tranferred. ', async (done) => {
+        app
+            .post('/package/create-package')
+            .send({ date_send: '2019-01-10', gps: 'La Plata, Buenos Aires, Argentina' })
+            .except(201)
+            .end(async (err) => {
+                if (err) throw err;
+                except(201).toBe();
+            });
     });
-
 });
